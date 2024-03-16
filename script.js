423f1fefe6cee9f497f788f74ea0baf1ff75f892
Fenixz666 <jppcamargo2@outlook.com>
2024-03-16 00:13:14 -0300

    draw teste

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function toggleTooltip(index) {
-    var tooltip = document.getElementById("tooltip" + index);
-    if (tooltip.style.visibility === "visible") {
-        tooltip.style.visibility = "hidden";
-        tooltip.style.opacity = "0";
-    } else {
-        tooltip.style.visibility = "visible";
-        tooltip.style.opacity = "1";
+function changeBackground(button) {
+    // Extrai o número do ID do botão
+    var buttonId = button.id;
+    var buttonNumber = buttonId.split('-')[2];
+
+    // Define o URL da imagem de fundo com base no número do botão
+    var imageUrl = '';
+    switch(buttonNumber) {
+        case '1':
+            imageUrl = 'https://static.wikia.nocookie.net/leagueoflegends/images/4/43/Fiddlesticks_OriginalCircle.png';
+            break;
+        case '2':
+            imageUrl = 'https://static.wikia.nocookie.net/leagueoflegends/images/a/ab/Fiddlesticks_SpectralCircle.png';
+            break;
+        // Adicione mais casos conforme necessário
     }
+
+    // Altera o plano de fundo do corpo da página
+    document.body.style.backgroundImage = 'url("' + imageUrl + '")';
 }
