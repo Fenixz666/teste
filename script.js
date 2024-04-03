954e67486792df50e9bbb5cae7b44aedf027ddf4
Fenixz666 <jppcamargo2@outlook.com>
2024-04-02 21:57:17 -0300

    draw

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.classList.add("show");
+window.addEventListener('scroll', function() {
+    var scrollTop = window.scrollY;
+    var loreSection = document.getElementById('Lore');
+    var nav = document.querySelector('nav');
+
+    // Se a posição do scroll for maior ou igual à posição do topo da seção "Lore"
+    if (scrollTop >= loreSection.offsetTop) {
+        nav.style.backgroundColor = 'rgba(255, 255, 255, 1)'; // Muda a cor de fundo do 'Saiba mais' para cobrir a página
+    } else {
+        nav.style.backgroundColor = 'rgba(255, 255, 255, 0.5)'; // Mantém a cor de fundo do 'Saiba mais' transparente
+    }
 });
